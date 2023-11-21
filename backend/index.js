39e5468cba6fb7ec8719c84dbb95c784b366ea4d
SManeesha99 <it20649012@my.sliit.lk>
2023-11-21 09:43:33 +0530

    Add Image
    Add Image

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(cors({credentials : true,origin : 'http://localhost:3000'}));
 app.use(cookieParser());
 
 // accept JSONS
-app.use(express.json());
+app.use(express.json({limit : "100mb"}));
 
 // config the urlEncoded middleware
 app.use(express.urlencoded({extended : false}));
